22870e903b51135d1bfeb4e08f6b8475cd1bff76
reowszer <reowsze@gmail.com>
2021-04-07 23:36:54 +0800

    updated databaseFunc.js

--- Gathable/databaseFunc.js
+++ Gathable/databaseFunc.js
@@ -1,17 +1,89 @@
-function init() {}
+var mysql = require('mysql')
+var con = null
+
+function init() {
+    con = mysql.reateConnection({
+        host: 'localhost',
+        port: '8889',
+        user: 'root',
+        password: '123',
+        database: 'documentario',
+        multipleStatements: true
+      });
+}
 
 // Accounts related
-function usernameExist(username) {return true; return false}
+function usernameExist(username) {
+    con.query('SELECT uname FROM event_data WHERE uname = ?',
+        [username],
+        function (err, result, fields) {
+            if (err) throw err
+            if (result.length > 0)
+                return true
+            else
+                return false
+    })
+}
 
-function addNewUser(username, pwHash, email, randHash) {}
+function addNewUser(username, pwHash, email, randHash) {
+    con.query('INSERT INTO user_info VALUES (?, ?, ?, ?)',
+        [username, pwHash, email, 0, randHash],
+        function (err, result, fields) {
+            if (err) throw err
+            if (results.length > 0)
+                console.log('add new user success')
+            else
+                console.log('add new user failed')
+    })
+}
 
-function verifyAccount(username, emailHash) { return true; return false }   // for new user
+function verifyAccount(username, emailHash) {
+    con.query('SELECT register_code FROM user_info WHERE uname = ?',
+        [username],
+        function (err, result, fields) {
+            if (err) throw err
+            if (result.length && emailHash == result[0])
+                return true
+            else
+                return false
+    })
+}   // for new user
 
-function verifyReset(username, emailHash) { return true; return false }     // for reset password
+function verifyReset(username, emailHash) {
+    con.query('SELECT uname FROM user_info WHERE uname = ? AND register_code = ?',
+        [username, emailHash],
+        function (err, result, fields) {
+            if (err) throw err
+            if (result.length > 0)
+                return true
+            else
+                return false
+    })
+}     // for reset password
 
-function verifyLogin(username, pwHash) { return true; return false}
+function verifyLogin(username, pwHash) {
+    con.query('SELECT uname FROM user_info WHERE uname = ? AND a_password = ?',
+        [username, pwHash],
+        function (err, result, fields) {
+            if (err) throw err
+            if (result.length > 0)
+                return true
+            else
+                return false
+    })
+}
 
-function removeAccount(username) {}
+function removeAccount(username) {
+    con.query('DELETE FROM user_info WHERE uname = ?',
+        [username],
+        function (err, result, fields) {
+            if (err) throw err
+            if (results.length > 0)
+                console.log('delete user success')
+            else
+                console.log('delete user failed')
+    })
+}
 
 module.exports = { init, usernameExist, addNewUser, verifyLogin }    // have to export like this
 
