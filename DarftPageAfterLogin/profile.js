a27c480506e1c70f5bd26473fc8856f3fa122963
楓LOKED <62502174+LOKaEDe@users.noreply.github.com>
2021-04-11 17:42:58 +0800

    for showing profile data test

---
+++ DarftPageAfterLogin/profile.js
@@ -0,0 +1,7 @@
+let uId = "80026513"
+let uName = "Onion Knight"
+let uEmail = "OKdark80@example.com"
+
+document.getElementById("uid").innerHTML = uId;
+document.getElementById("uname").innerHTML = uName;
+document.getElementById("uemail").innerHTML = uEmail;
